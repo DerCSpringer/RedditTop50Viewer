ff37a498543c722cb12a9fc218392307abc3cd61
Daniel Springer <dcs@Daniels-MacBook-Pro.local>
2017-08-24 12:39:40 -0700

    added postsVC

---
+++ Controllers/PostsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PostsViewController.swift
+//  RedditTop50Viewer
+//
+//  Created by Daniel Springer on 8/24/17.
+//  Copyright © 2017 Daniel Springer. All rights reserved.
+//
+
+import Foundation
