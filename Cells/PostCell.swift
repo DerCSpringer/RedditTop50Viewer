94e50e5810c653e1e7dc756744a55d11a39c0a4f
Daniel Springer <dcs@Daniels-MacBook-Pro.local>
2017-08-28 12:45:13 -0700

    added a configure for the cell and added default picture for thumbnail if none is found

--- Cells/PostCell.swift
+++ Cells/PostCell.swift
@@ -15,6 +15,28 @@ class PostsCell: UITableViewCell {
     @IBOutlet weak var thumbnail: UIImageView!
     var didTouchThumbnail: (() -> Void)?
     
+    func configureWith(entry:Post?) {
+        if let entry = entry {
+            self.author.text = "by \(entry.author)"
+            self.numOfComments.text = "\(String(entry.numberOfComments)) comments"
+            self.date.text = "posted \(entry.datePosted.timeFromNow())"
+            self.postTitle.text = entry.title
+            if entry.thumbnail != nil {
+                if let image = entry.imageData {
+                    thumbnail.image = UIImage(data: image)
+                    self.thumbnail.isUserInteractionEnabled = true
+                    setupGestureForThumbnail()
+                }
+            } else {
+                self.thumbnail.isUserInteractionEnabled = false
+                thumbnail.image = UIImage(named: "default-thumbnail.jpg")
+                //maybe do something here
+                //reuse might be causing issues
+                // I need to check that cells are being resued properly
+            }
+        }
+    }
+    
     func setupGestureForThumbnail() {
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTouch(gesture:)))
         self.thumbnail.addGestureRecognizer(tapGesture)
