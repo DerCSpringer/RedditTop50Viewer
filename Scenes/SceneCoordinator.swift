7271421dc6c3b6ddb8952e7ffa4b4aa1dd061a44
Daniel Springer <dcs@Daniels-MacBook-Pro.local>
2017-08-28 11:35:07 -0700

    added init and variables

--- Scenes/SceneCoordinator.swift
+++ Scenes/SceneCoordinator.swift
@@ -9,5 +9,22 @@
 import UIKit
 
 class SceneCoordinator: SceneCoordinatorType {
+    
+    fileprivate var window: UIWindow
+    fileprivate var currentViewController: UIViewController
+    
+    required init(window: UIWindow) { //If we pass in this window we can get it's root VC
+        self.window = window
+        currentViewController = window.rootViewController!
+    }
+    
+    //Gets the VC instead of the Navigation VC
+    static func actualViewController(for viewController: UIViewController) -> UIViewController {
+        if let navigationController = viewController as? UINavigationController {
+            return navigationController.viewControllers.first!
+        } else {
+            return viewController
+        }
+    }
 
 }
