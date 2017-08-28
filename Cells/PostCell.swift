6fe0a679d047833d33bb57f321425b42762d57d7
Daniel Springer <dcs@Daniels-MacBook-Pro.local>
2017-08-28 12:39:18 -0700

    hooked up outlets for labels and setup gesture for thumbnail touch

--- Cells/PostCell.swift
+++ Cells/PostCell.swift
@@ -8,5 +8,19 @@
 
 import UIKit
 class PostsCell: UITableViewCell {
+    @IBOutlet weak var postTitle: UILabel!
+    @IBOutlet weak var numOfComments: UILabel!
+    @IBOutlet weak var author: UILabel!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var thumbnail: UIImageView!
+    var didTouchThumbnail: (() -> Void)?
     
+    func setupGestureForThumbnail() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTouch(gesture:)))
+        self.thumbnail.addGestureRecognizer(tapGesture)
+    }
+    
+    func didTouch(gesture: UIGestureRecognizer) {
+        self.didTouchThumbnail!()
+    }
 }
