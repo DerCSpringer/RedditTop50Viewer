d7ccc9d2d70d01f675e1bf76cc6569f1d5675169
Daniel Springer <dcs@Daniels-MacBook-Pro.local>
2017-08-28 12:35:32 -0700

    added PostCell class and setup auto layout constraints and labels in storyboard

---
+++ Cells/PostCell.swift
@@ -0,0 +1,12 @@
+//
+//  PostCell.swift
+//  RedditTop50Viewer
+//
+//  Created by Daniel Springer on 8/28/17.
+//  Copyright © 2017 Daniel Springer. All rights reserved.
+//
+
+import UIKit
+class PostsCell: UITableViewCell {
+    
+}
